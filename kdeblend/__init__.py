65c287be64ad7caa64d865b53ea16b9ad54a9260
Erin Sheldon <erin.sheldon@gmail.com>
2026-07-21 11:29:45 -0400

    rename deblend.py module to deblender.py
    so we can import module and function

--- kdeblend/__init__.py
+++ kdeblend/__init__.py
@@ -1,11 +1,12 @@
 from ._version import __version__
 
-from .deblend import (
+from . import deblender
+from .deblender import (
     deblend, deblend_stamps,
     DEBLENDED_AS_PSF, RESTARTED, EXTERNALS_SUBTRACTED,
 )
 
 __all__ = [
-    '__version__', 'deblend', 'deblend_stamps',
+    '__version__', 'deblender', 'deblend', 'deblend_stamps',
     'DEBLENDED_AS_PSF', 'RESTARTED', 'EXTERNALS_SUBTRACTED',
 ]
